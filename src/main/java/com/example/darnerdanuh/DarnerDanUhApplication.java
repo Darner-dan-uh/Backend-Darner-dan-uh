91f6c9477d88683481448d2ac91f5a0246fbaad8
kanghanna1202 <namnam1178@daum.net>
2020-10-25 21:11:45 +0900

    build: create project

---
+++ src/main/java/com/example/darnerdanuh/DarnerDanUhApplication.java
@@ -0,0 +1,13 @@
+package com.example.darnerdanuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarnerDanUhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarnerDanUhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/darnerdanuh/DarnerDanUhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.darnerdanuh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarnerDanUhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
