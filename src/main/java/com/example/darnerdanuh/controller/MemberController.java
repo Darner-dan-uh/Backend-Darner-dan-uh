1bd05e2c78fa3492cf856f17e85e7fc30ae7038d
kanghanna1202 <namnam1178@daum.net>
2020-10-29 11:41:01 +0900

    feature: send email

--- src/main/java/com/example/darnerdanuh/controller/MemberController.java
+++ src/main/java/com/example/darnerdanuh/controller/MemberController.java
@@ -4,7 +4,10 @@ import com.example.darnerdanuh.domain.ResponseJson;
 import com.example.darnerdanuh.domain.member.Member;
 import com.example.darnerdanuh.domain.member.MemberDto;
 import com.example.darnerdanuh.domain.member.MemberRepository;
+import com.example.darnerdanuh.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,14 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberController {
 
-    final MemberRepository memberRepository;
-    final PasswordEncoder encode;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder encode;
 
+    private final EmailService service;
+
+    @SneakyThrows
     @PostMapping("/signup")
     public ResponseJson saveMember(@RequestBody MemberDto memberDto) {
         memberRepository.save(Member.createMember(memberDto.getUserId(),
                 memberDto.getName(), memberDto.getEmail(),
                 encode.encode(memberDto.getPassword())));
+        service.sendSimpleMessage(memberDto.getEmail());
 
         ResponseJson json = new ResponseJson();
         json.setMessage("success");
