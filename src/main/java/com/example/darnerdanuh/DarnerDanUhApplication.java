2cb01d0435dcf22e9ec9ef11125b630f6af21ea5
sonohoshi <dev.sonohoshi@gmail.com>
2020-11-27 10:13:20 +0900

    Make api /rank

--- src/main/java/com/example/darnerdanuh/DarnerDanUhApplication.java
+++ src/main/java/com/example/darnerdanuh/DarnerDanUhApplication.java
@@ -1,5 +1,6 @@
 package com.example.darnerdanuh;
 
+import com.example.darnerdanuh.service.RankingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/example/darnerdanuh/controller/MemberController.java
+++ src/main/java/com/example/darnerdanuh/controller/MemberController.java
@@ -4,14 +4,12 @@ import com.example.darnerdanuh.domain.ResponseJson;
 import com.example.darnerdanuh.domain.member.MemberDto;
 import com.example.darnerdanuh.domain.member.MemberRepository;
 import com.example.darnerdanuh.domain.member.VerifyInfo;
+import com.example.darnerdanuh.service.RankingService;
 import com.example.darnerdanuh.service.SignUpService;
 import com.example.darnerdanuh.service.VerifyCodeService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +17,8 @@ public class MemberController {
     private final VerifyCodeService verifyCodeService;
 
     private final SignUpService signUpService;
+
+    private final RankingService rankingService;
 
     @PostMapping("/register")
     public ResponseJson saveMember(@RequestBody MemberDto memberDto) {
@@ -28,5 +28,10 @@ public class MemberController {
     @PostMapping("/verifywithemail")
     public ResponseJson verifyCode(@RequestBody VerifyInfo verifyInfo){
         return verifyCodeService.setVerifyCode(verifyInfo);
+    }
+
+    @GetMapping("/rank")
+    public String getRanking(@RequestParam(value = "count") int count){
+        return rankingService.getRanking(count);
     }
 }
